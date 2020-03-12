029d7f7e2d3ddb9216b6406630519c35a3eeb76c
Denis Timofeev <y719xy@gmail.com>
2020-03-12 21:16:53 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int current = 2_000_000_000;
+        int transfer = 500_000_000;
+        int total = current + transfer;
+        System.out.println(total);
+    }
+}
